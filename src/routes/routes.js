3cf221eb643e5c38153db7d4ba7653e643c00214
Douglas Bernardes <douglaspb96@gmail.com>
2019-12-09 17:27:43 -0300

    Adicionado webpack

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,13 +3,14 @@ const { apolloUploadExpress } = require('apollo-upload-server');
 const schema = require('../api/schema');
 const { isDevelopment } = require('../config/server');
 const routes = require('express').Router();
-const BASE_ROUTE = '/api/';
+const BASE_ROUTE = !isDevelopment ? '/api/' : "/"; //TEMPORÁRIO
+
 routes.get(BASE_ROUTE, (req, res) => {
     res.send('API');
 });
 
 routes.post(
-    BASE_ROUTE+'/api', 
+    BASE_ROUTE+'api', 
     apolloUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }),
     graphqlHTTP({
         schema: schema,
@@ -17,7 +18,7 @@ routes.post(
     })
 );
 if(isDevelopment){
-    routes.get(BASE_ROUTE+'/api', graphqlHTTP({
+    routes.get(BASE_ROUTE+'api', graphqlHTTP({
         schema: schema,
         graphiql: true
     }));
