3713ec91cca36a9f78adf3f5216951a0de332e46
Douglas Bernardes <douglaspb96@gmail.com>
2019-12-09 20:07:05 -0300

    Update path to production

--- src/config/paths.js
+++ src/config/paths.js
@@ -1,9 +1,12 @@
+// Update with your config settings.
+const dotenv = require('dotenv');
+dotenv.config();
+
 const path = require('path');
-const { isDevelopment } = require('../config/server');
-const PATH_IMAGES = isDevelopment ? path.join(__dirname, '..', '..', '..', 'images_api') : "/var/www/images_api/";
+const PATH_IMAGES = path.join(__dirname, '..', '..', '..', 'images_api');
 
 module.exports = {
-    PATH_IMAGES : PATH_IMAGES,
+    PATH_IMAGES : process.env.PATH_IMAGES || PATH_IMAGES,
     PATH_BAG_IMAGES : path.join(PATH_IMAGES, 'bags'),
     PATH_MEDIA_IMAGES : path.join(PATH_IMAGES, 'medias'),
     PATH_OTHERS_IMAGES : path.join(PATH_IMAGES, 'others')
