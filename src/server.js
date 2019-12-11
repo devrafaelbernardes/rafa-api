ff80f196866ef010e406e60e9dc250a3a047ca5f
Douglas Bernardes <douglaspb96@gmail.com>
2019-12-11 19:38:54 -0300

    Finalizada migração

--- src/server.js
+++ src/server.js
@@ -7,19 +7,19 @@ const { BASE_ROUTE, isDevelopment } = require('./config/server');
 const { PATH_BAG_IMAGES, PATH_MEDIA_IMAGES, PATH_OTHERS_IMAGES } = require('./config/paths');
 
 const app = express();
-if(!isDevelopment){
+/*if(!isDevelopment){
     app.use(cors({
         origin: ["https://rbernardes.com.br/", "https://www.rbernardes.com.br/"],
         methods: ["GET", "POST"],
         allowedHeaders: ["Content-Type", "Origin", "X-Requested-With", "Accept"]
     }));
-}else{
+}else{*/
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
-}
+//}
 
 app.use(BASE_ROUTE+'image/bag/', express.static(PATH_BAG_IMAGES));
 app.use(BASE_ROUTE+'image/media/', express.static(PATH_MEDIA_IMAGES));
