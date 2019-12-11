ae6151f958cfe4dfd70f9753c27da0a847499042
Douglas Bernardes <douglaspb96@gmail.com>
2019-12-11 19:43:57 -0300

    Finalizada migração

--- src/config/server.js
+++ src/config/server.js
@@ -5,7 +5,7 @@ dotenv.config();
 const PORT = 4040;
 const isDevelopment = process.env.NODE_ENV !== "production";
 const BASE_ROUTE = "/";
-const LINK_SERVER = isDevelopment ? 'http://localhost:'+PORT+'/' : 'http://www.api.rbernardes.com.br'+BASE_ROUTE;
+const LINK_SERVER = isDevelopment ? 'http://localhost:'+PORT+'/' : 'https://www.api.rbernardes.com.br'+BASE_ROUTE;
 const LINK_IMAGES = LINK_SERVER+'image/';
 
 module.exports = {
