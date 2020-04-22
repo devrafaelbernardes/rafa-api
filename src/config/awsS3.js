939195de1b472359a77380261879910d8d7d8140
Douglas Bernardes <douglaspb96@gmail.com>
2020-04-22 01:06:19 -0300

    Um dia de duração dos links temporarios

--- src/config/awsS3.js
+++ src/config/awsS3.js
@@ -31,7 +31,7 @@ export const getSignedUrl = ({ url, name, isPrivate = false }) => {
             return awsS3.getSignedUrl('getObject', {
                 Bucket,
                 Key: name,
-                Expires: 300 // 5min expires
+                Expires: 3600 // 5min expires
             });
         } catch (error) {}
     }
