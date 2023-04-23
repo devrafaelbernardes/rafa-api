7873acae2d2f6b005a59879b87199c3e2fc18425
Douglas Bernardes <douglaspb96@gmail.com>
2023-04-23 11:04:59 -0300

    add

--- src/classes/models/Upload.js
+++ src/classes/models/Upload.js
@@ -1,194 +1,225 @@
-import path from 'path';
-import fs from 'fs';
-import { isDevelopment, LINK_IMAGES, LINK_VIDEOS, LINK_MATERIALS, LINK_MODELING } from '../../config/server';
-import { PATH_IMAGES, PATH_VIDEOS, PATH_MATERIAL, PATH_MODELING } from '../../config/paths';
-import { clientVimeo, getSignedUrl } from '../../config/vimeo';
+import path from "path";
+import fs from "fs";
+import {
+  isDevelopment,
+  LINK_IMAGES,
+  LINK_VIDEOS,
+  LINK_MATERIALS,
+  LINK_MODELING,
+} from "../../config/server";
+import {
+  PATH_IMAGES,
+  PATH_VIDEOS,
+  PATH_MATERIAL,
+  PATH_MODELING,
+} from "../../config/paths";
+import { clientVimeo, getSignedUrl } from "../../config/vimeo";
 
 export const Upload = () => {
-    const TYPES_UPLOAD = {
-        IMAGE: 1,
-        VIDEO: 2,
-        MATERIAL: 3,
-        MODELING: 4,
-    };
+  const TYPES_UPLOAD = {
+    IMAGE: 1,
+    VIDEO: 2,
+    MATERIAL: 3,
+    MODELING: 4,
+  };
 
-    const imageTypes = ["image/gif", "image/png", "image/jpg", "image/jpeg"];
-    const videoTypes = ["video/mp4"];
-    const materialTypes = ["application/pdf"];
-    const modelingTypes = ["application/pdf"];
+  const imageTypes = ["image/gif", "image/png", "image/jpg", "image/jpeg"];
+  const videoTypes = ["video/mp4"];
+  const materialTypes = ["application/pdf"];
+  const modelingTypes = ["application/pdf"];
 
-    const defineFiletype = (type) => {
-        const types = [...imageTypes, ...videoTypes, ...materialTypes, ...modelingTypes];
-        if (types.includes(type)) {
-            return type.split("/").pop();
-        }
-        return null;
+  const defineFiletype = (type) => {
+    const types = [
+      ...imageTypes,
+      ...videoTypes,
+      ...materialTypes,
+      ...modelingTypes,
+    ];
+    if (types.includes(type)) {
+      return type.split("/").pop();
     }
+    return null;
+  };
 
-    const justImage = (type) => imageTypes.includes(type);
-    const justMaterial = (type) => materialTypes.includes(type);
-    const justModeling = (type) => modelingTypes.includes(type);
-    const justVideo = (type) => videoTypes.includes(type);
+  const justImage = (type) => imageTypes.includes(type);
+  const justMaterial = (type) => materialTypes.includes(type);
+  const justModeling = (type) => modelingTypes.includes(type);
+  const justVideo = (type) => videoTypes.includes(type);
 
-    const defineFilename = (name) => `file${Date.now()}-${String(name).replace(/[^A-Za-z0-9.]+/g, "")}`;
+  const defineFilename = (name) =>
+    `file${Date.now()}-${String(name).replace(/[^A-Za-z0-9.]+/g, "")}`;
 
-    const getUrl = async (name, typeUpload) => {
-        if (name && Object.values(TYPES_UPLOAD).includes(typeUpload)) {
-            let isPrivate = false;
-            let url = null;
-            if (typeUpload === TYPES_UPLOAD.VIDEO) {
-                isPrivate = !isDevelopment;
-                url = LINK_VIDEOS;
-            } else if (typeUpload === TYPES_UPLOAD.IMAGE) {
-                url = LINK_IMAGES;
-            } else if (typeUpload === TYPES_UPLOAD.MATERIAL) {
-                url = LINK_MATERIALS;
-            } else if (typeUpload === TYPES_UPLOAD.MODELING) {
-                url = LINK_MODELING;
-            }
-            return getSignedUrl({ url, videoId: name, isPrivate });
-        }
-        return null;
+  const getUrl = async (name, typeUpload) => {
+    if (name && Object.values(TYPES_UPLOAD).includes(typeUpload)) {
+      let isPrivate = false;
+      let url = null;
+      if (typeUpload === TYPES_UPLOAD.VIDEO) {
+        isPrivate = !isDevelopment;
+        url = LINK_VIDEOS;
+      } else if (typeUpload === TYPES_UPLOAD.IMAGE) {
+        url = LINK_IMAGES;
+      } else if (typeUpload === TYPES_UPLOAD.MATERIAL) {
+        url = LINK_MATERIALS;
+      } else if (typeUpload === TYPES_UPLOAD.MODELING) {
+        url = LINK_MODELING;
+      }
+      return getSignedUrl({ url, videoId: name, isPrivate });
     }
+    return "";
+  };
 
-    const getVideoUrl = (name) => getUrl(name, TYPES_UPLOAD.VIDEO);
-    const getImageUrl = (name) => getUrl(name, TYPES_UPLOAD.IMAGE);
-    const getMaterialUrl = (name) => getUrl(name, TYPES_UPLOAD.MATERIAL);
-    const getModelingUrl = (name) => getUrl(name, TYPES_UPLOAD.MODELING);
+  const getVideoUrl = (name) => getUrl(name, TYPES_UPLOAD.VIDEO);
+  const getImageUrl = (name) => getUrl(name, TYPES_UPLOAD.IMAGE);
+  const getMaterialUrl = (name) => getUrl(name, TYPES_UPLOAD.MATERIAL);
+  const getModelingUrl = (name) => getUrl(name, TYPES_UPLOAD.MODELING);
 
-    const storeLocal = ({ stream, filename, type, typeUpload }) => {
-        return new Promise(async (resolve, reject) => {
-            if (!type) {
-                reject(null);
-            }
-            let name = await defineFilename(filename);
-            //name = `${name}.${type}`;
+  const storeLocal = ({ stream, filename, type, typeUpload }) => {
+    return new Promise(async (resolve, reject) => {
+      if (!type) {
+        reject(null);
+      }
+      let name = await defineFilename(filename);
+      //name = `${name}.${type}`;
 
-            let resultPath = null;
-            if (typeUpload === TYPES_UPLOAD.VIDEO) {
-                resultPath = PATH_VIDEOS;
-            } else if (typeUpload === TYPES_UPLOAD.IMAGE) {
-                resultPath = PATH_IMAGES;
-            } else if (typeUpload === TYPES_UPLOAD.MATERIAL) {
-                resultPath = PATH_MATERIAL;
-            } else if (typeUpload === TYPES_UPLOAD.MODELING) {
-                resultPath = PATH_MODELING;
-            }
-            resultPath = path.resolve(resultPath, name);
+      let resultPath = null;
+      if (typeUpload === TYPES_UPLOAD.VIDEO) {
+        resultPath = PATH_VIDEOS;
+      } else if (typeUpload === TYPES_UPLOAD.IMAGE) {
+        resultPath = PATH_IMAGES;
+      } else if (typeUpload === TYPES_UPLOAD.MATERIAL) {
+        resultPath = PATH_MATERIAL;
+      } else if (typeUpload === TYPES_UPLOAD.MODELING) {
+        resultPath = PATH_MODELING;
+      }
+      resultPath = path.resolve(resultPath, name);
 
-            return stream
-                .on('error', error => {
-                    if (stream.truncated)
-                        // delete the truncated file
-                        fs.unlinkSync(resultPath);
-                    reject(error);
-                })
-                .pipe(fs.createWriteStream(resultPath))
-                .on('error', error => reject(error))
-                .on('finish', () => resolve({ path: resultPath, url: getUrl(name, typeUpload), filename: name }));
-        });
-    }
+      return stream
+        .on("error", (error) => {
+          if (stream.truncated)
+            // delete the truncated file
+            fs.unlinkSync(resultPath);
+          reject(error);
+        })
+        .pipe(fs.createWriteStream(resultPath))
+        .on("error", (error) => reject(error))
+        .on("finish", () =>
+          resolve({
+            path: resultPath,
+            url: getUrl(name, typeUpload),
+            filename: name,
+          })
+        );
+    });
+  };
 
-    const storeVimeo = async ({ absolutePath, name, description }) => {
+  const storeVimeo = async ({ absolutePath, name, description }) => {
+    try {
+      //const fileStats = await fs.statSync(absolutePath);
+      const params = {
+        name,
+        description,
+      };
+
+      return new Promise(async (resolve, reject) => {
         try {
-            //const fileStats = await fs.statSync(absolutePath);
-            const params = {
-                name,
-                description,
-            };
-
-            return new Promise(async (resolve, reject) => {
-                try {
-                    await clientVimeo.upload(
-                        absolutePath,
-                        params,
-                        async (data) => {
-                            if (!data) {
-                                reject("ERROR UPLOAD");
-                            }
-                            // data = /videos/:id
-                            const dataSplit = String(data).split('/');
-                            if (dataSplit.length >= 3) {
-                                const [, , id] = dataSplit;
-                                const url = await getVideoUrl(id);
-                                resolve({
-                                    url,
-                                    filename: id,
-                                });
-                            }
-                            resolve(null);
-                        },
-                        () => { },
-                        (err) => {
-                            reject(err);
-                        }
-                    );
-                } catch (error) {
-                    reject(null);
-                }
-            });
-        } catch (error) { }
-        return null;
-    }
-
-    const upload = async ({ file, name, description, typeUpload = TYPES_UPLOAD.IMAGE }) => {
-        if (file && Object.values(TYPES_UPLOAD).includes(typeUpload)) {
-            const elementFile = await file;
-            if (elementFile) {
-                let isPrivate = false;
-                const { createReadStream, filename, mimetype, encoding } = elementFile;
-
-                const stream = createReadStream();
-                const type = defineFiletype(mimetype);
-
-                if (typeUpload === TYPES_UPLOAD.VIDEO) {
-                    if (!justVideo(mimetype)) {
-                        return null;
-                    }
-                    if (!isDevelopment) {
-                        return storeVimeo({
-                            absolutePath: stream && stream.path,
-                            name,
-                            description
-                        });
-                    }
-                } else if (typeUpload === TYPES_UPLOAD.MATERIAL) {
-                    if (!justMaterial(mimetype)) {
-                        return null;
-                    }
-                    isPrivate = true;
-                } else if (typeUpload === TYPES_UPLOAD.MODELING) {
-                    if (!justModeling(mimetype)) {
-                        return null;
-                    }
-                } else {
-                    if (!justImage(mimetype)) {
-                        return null;
-                    }
-                }
-
-                if (type) {
-                    return storeLocal({ stream, filename, type, isPrivate, typeUpload });
-                }
+          await clientVimeo.upload(
+            absolutePath,
+            params,
+            async (data) => {
+              if (!data) {
+                reject("ERROR UPLOAD");
+              }
+              // data = /videos/:id
+              const dataSplit = String(data).split("/");
+              if (dataSplit.length >= 3) {
+                const [, , id] = dataSplit;
+                const url = await getVideoUrl(id);
+                resolve({
+                  url,
+                  filename: id,
+                });
+              }
+              resolve(null);
+            },
+            () => {},
+            (err) => {
+              reject(err);
             }
+          );
+        } catch (error) {
+          reject(null);
         }
-        return null;
-    }
+      });
+    } catch (error) {}
+    return null;
+  };
 
-    return {
-        TYPES_UPLOAD,
-        defineFiletype,
-        defineFilename,
-        getVideoUrl,
-        getImageUrl,
-        getMaterialUrl,
-        getModelingUrl,
-        upload,
-        uploadImage: (file) => upload({ file, typeUpload: TYPES_UPLOAD.IMAGE }),
-        uploadVideo: (file, name, description) => upload({ file, name, description, typeUpload: TYPES_UPLOAD.VIDEO }),
-        uploadMaterial: (file) => upload({ file, typeUpload: TYPES_UPLOAD.MATERIAL }),
-        uploadModeling: (file) => upload({ file, typeUpload: TYPES_UPLOAD.MODELING }),
+  const upload = async ({
+    file,
+    name,
+    description,
+    typeUpload = TYPES_UPLOAD.IMAGE,
+  }) => {
+    if (file && Object.values(TYPES_UPLOAD).includes(typeUpload)) {
+      const elementFile = await file;
+      if (elementFile) {
+        let isPrivate = false;
+        const { createReadStream, filename, mimetype, encoding } = elementFile;
+
+        const stream = createReadStream();
+        const type = defineFiletype(mimetype);
+
+        if (typeUpload === TYPES_UPLOAD.VIDEO) {
+          if (!justVideo(mimetype)) {
+            return null;
+          }
+          if (!isDevelopment) {
+            return storeVimeo({
+              absolutePath: stream && stream.path,
+              name,
+              description,
+            });
+          }
+        } else if (typeUpload === TYPES_UPLOAD.MATERIAL) {
+          if (!justMaterial(mimetype)) {
+            return null;
+          }
+          isPrivate = true;
+        } else if (typeUpload === TYPES_UPLOAD.MODELING) {
+          if (!justModeling(mimetype)) {
+            return null;
+          }
+        } else {
+          if (!justImage(mimetype)) {
+            return null;
+          }
+        }
+
+        if (type) {
+          return storeLocal({ stream, filename, type, isPrivate, typeUpload });
+        }
+      }
     }
+    return null;
+  };
+
+  return {
+    TYPES_UPLOAD,
+    defineFiletype,
+    defineFilename,
+    getVideoUrl,
+    getImageUrl,
+    getMaterialUrl,
+    getModelingUrl,
+    upload,
+    uploadImage: (file) => upload({ file, typeUpload: TYPES_UPLOAD.IMAGE }),
+    uploadVideo: (file, name, description) =>
+      upload({ file, name, description, typeUpload: TYPES_UPLOAD.VIDEO }),
+    uploadMaterial: (file) =>
+      upload({ file, typeUpload: TYPES_UPLOAD.MATERIAL }),
+    uploadModeling: (file) =>
+      upload({ file, typeUpload: TYPES_UPLOAD.MODELING }),
+  };
 };
 
 export default Upload;
